3b50bb60096af8dc56876e50371b7c730f6061bd
fonsternberg <rmnfnstrnbrg@gmail.com>
2026-02-04 21:43:42 +0300

    modified get

--- responces/get.go
+++ responces/get.go
@@ -3,6 +3,7 @@ package responces
 import (
 	"database/sql"
 	"log"
+	"github.com/voyadger01/rest-contacts_golang_01/contact"
 )
 
 func GetOne(db *sql.DB, dbname string, id int) {
@@ -10,5 +11,15 @@ func GetOne(db *sql.DB, dbname string, id int) {
 	if err != nil {
 		log.Fatalln("bad query")		
 	}
-	
+	defer rows.Close()
+	contacts := []contact.Contact{}
+	for rows.Next(){
+		cnt := contact.Contact{}
+		err := rows.Scan(&cnt.Name, &cnt.Phone)
+		if err != nil {
+			log.Println("bad name or phone")
+			continue
+		}
+		contacts = append(contacts, cnt)
+	}
 }
