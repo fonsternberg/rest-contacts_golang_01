ead59313b731c1fa30ac32627cd17053b52b9866
fonsternberg <rmnfnstrnbrg@gmail.com>
2026-02-05 23:55:18 +0300

    fixes

--- responces/put.go
+++ responces/put.go
@@ -8,7 +8,7 @@ import (
 	"github.com/voyadger01/rest-contacts_golang_01/contact"
 )
 
-func isThereId(db *sql.DB, dbname string, cnt contact.Contact, id int) bool {
+func isThereId(db *sql.DB, dbname string, cnt contact.Contact) bool {
 	rows, err := db.Query("SELECT * FROM ", dbname)
 	if err != nil {
 		log.Fatalln("bad num of rows")
@@ -18,7 +18,7 @@ func isThereId(db *sql.DB, dbname string, cnt contact.Contact, id int) bool {
 		log.Fatalln("bad num of columns")
 	}
 	isId := false
-	for i := 0; i < len(cols); i++ {
+	for i := range cols {
 		colsName, err := strconv.Atoi(cols[i])
 		if err != nil {
 			log.Fatalln("bad format of cols")
@@ -31,7 +31,7 @@ func isThereId(db *sql.DB, dbname string, cnt contact.Contact, id int) bool {
 }
 
 func Put(db *sql.DB, dbname string, cnt contact.Contact, id int) {
-	isId := isThereId(db, dbname, cnt, id)
+	isId := isThereId(db, dbname, cnt)
 	if isId {
 		Delete(db, dbname, id)
 		Post(db, dbname, cnt)
