56db30be81e042ed659ac326e1c14e4581cf104e
fonsternberg <rmnfnstrnbrg@gmail.com>
2026-02-06 22:57:06 +0300

    go to http: post

--- handler/post.go
+++ handler/post.go
@@ -1,16 +1,25 @@
 package handler
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/voyadger01/rest-contacts_golang_01/structs"
 )
 
-func (s *Server) post(w http.ResponseWriter, r *http.Request, newcnt structs.Contact) {
-	result, err := s.DataB.Exec("INSERT INTO contacts VALUES", newcnt.Id, newcnt.Name, newcnt.Phone)
+func (s *Server) post(w http.ResponseWriter, r *http.Request, contact structs.Contact) {
+	result, err := s.DataB.Exec("INSERT INTO contacts (name, phone, email) VALUES (?, ?)", contact.Name, contact.Phone)
 	if err != nil {
-		http.Error(w, "can't insert values", http.StatusInternalServerError)
+		log.Println("DB insert error: ", err)
+		http.Error(w, "can't insert", http.StatusInternalServerError)
 	}
-	rowsCheck, _ := result.RowsAffected()
-	log.Println("Rows added: ", rowsCheck)
+	tempId, _ := result.LastInsertId()
+	contact.Id = int(tempId)
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(contact)
+	if err != nil {
+		http.Error(w, "encode error", http.StatusInternalServerError)
+	}
 }
